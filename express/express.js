30832116fa6f4967e0fbc22af43f77b2f648233f
mikk922r <mikk922r@edu.mercantec.dk>
2026-06-16 15:03:24 +0200

    Added endpoint for stations

--- express/express.js
+++ express/express.js
@@ -29,6 +29,23 @@ app.get("/weather", async (req, res) => {
 		});
 });
 
+app.get("/stations", async (req, res) => {
+	const response = await fetch("https://opendataapi.dmi.dk/v2/climateData/collections/station/items");
+
+	if (!response.ok) return res.status(500).send({ error: "Error fetching station data" });
+
+	const jsonResponse = await response.json();
+
+	const prettifiedResponse = jsonResponse.features
+		.filter((station) => station.properties.country === "DNK")
+		.map((station) => ({
+			id: station.properties.stationId,
+			name: station.properties.name,
+		}));
+
+	res.status(200).send(prettifiedResponse);
+});
+
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
 });
