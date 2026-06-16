a1daff13f0ef844030b6b988b77b90bdeedffe43
Andreas <andreas3x3@protonmail.com>
2026-06-16 12:03:27 +0200

    Better
    Better structure and added the express.js to the project

---
+++ express/express.js
@@ -0,0 +1,16 @@
+import express from 'express'
+const app = express()
+const port = 8080
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/test', (req, res) => {
+  res.send('Hello Pølse!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
