c6885d0fc5c65f4d991580c2cf9d3cfd02ab6a0d
mikk922r <mikk922r@edu.mercantec.dk>
2026-06-16 14:29:10 +0200

    Removed test endpoints

--- express/express.js
+++ express/express.js
@@ -1,39 +1,34 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+
 const app = express();
-const port = 8080;
+const port = 80;
 
-dotenv.config();
+app.get("/weather", async (req, res) => {
+	// ?parameterId=temp_dry&period=latest&stationId=06180
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+	const id = req.query.stationId;
 
-app.get('/test', (req, res) => {
-    res.send('Hello Pølse!')
-})
+	if (!id) {
+		return res.status(400).send("Missing stationId query parameter");
+	}
 
-app.get('/weather', async (req, res) => {
-    // ?parameterId=temp_dry&period=latest&stationId=06180
-    const id = req.query.stationId;
-    if (!id) {
-        res.status(400).send('Missing stationId query parameter');
-        return;
-    }
+	console.log(`Received request for stationId: ${id}`);
 
-    console.log(`Received request for stationId: ${id}`);
+	const url = `https://opendataapi.dmi.dk/v2/metObs/collections/observation/items?parameterId=temp_dry&period=latest&stationId=${id}`;
 
-    const url = `${process.env.API_CALL}?parameterId=temp_dry&period=latest&stationId=${id}`;
-    await fetch(url).then(async (response) => {
-        const jsonResponse = await response.json();
-        res.send(jsonResponse);
-    }).catch(error => {
-        console.error('Error fetching weather data:', error);
-        res.status(500).send('Error fetching weather data');
-    });
+	await fetch(url)
+		.then(async (response) => {
+			const jsonResponse = await response.json();
+
+			res.send(jsonResponse);
+		})
+		.catch((error) => {
+			console.error("Error fetching weather data:", error);
+
+			res.status(500).send("Error fetching weather data");
+		});
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
-
+	console.log(`Example app listening on port ${port}`);
+});
